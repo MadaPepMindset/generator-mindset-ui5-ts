41b39c2fda1e6f7ac1e163dffbe80157065e38c4
Madalin Peptine <madalin.peptine@mindsetgroup.it>
2024-04-23 11:48:43 +0200

    first push

---
+++ generators/app/index.js
@@ -0,0 +1,11 @@
+import Generator from "yeoman-generator";
+
+export default class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+    this.log('Initializing...');
+  }
+  start() {
+    this.log('Do something...');
+  }
+};
